6e5db9f738550c4e83f279c181173ea3aee764b0
Dmitri Pisarev <dimaip@gmail.com>
2018-10-05 13:14:58 +0300

    Even more design fixes

--- DistributionPackages/Sfi.Kateheo/Resources/Public/Frontend/js/vendor-lory.js
+++ DistributionPackages/Sfi.Kateheo/Resources/Public/Frontend/js/vendor-lory.js
@@ -3,63 +3,9 @@
 (function () {
 
     function Carousel(el) {
-        // State
-        var index;
-        var slidesToScroll;
-        var instance = lory(el, createSettings());
-
-        function createSettings() {
-            index = 0;
-            if (el.dataset.carouselPreset === 'announcements') {
-                var clientWidth = window.innerWidth;
-                slidesToScroll = 2;
-                if (clientWidth > 640) {
-                    slidesToScroll = 4;
-                }
-                if (clientWidth > 1200) {
-                    slidesToScroll = 5;
-                }
-            } else {
-                slidesToScroll = 1;
-            }
-            return {
-                slidesToScroll: slidesToScroll,
-                enableMouseEvents: true
-            };
-        }
-
-        function updateControls() {
-            const next = el.querySelector('.next');
-            const prev = el.querySelector('.prev');
-            next.classList.remove('disabled');
-            prev.classList.remove('disabled');
-
-            var totalItems = el.querySelectorAll('li').length;
-            if (totalItems - slidesToScroll <= slidesToScroll * index) {
-                next.classList.add('disabled');
-            }
-            if (index === 0) {
-                prev.classList.add('disabled');
-            }
-        }
-
-        updateControls();
-
-        el.addEventListener('on.lory.resize', function () {
-            instance.setup(createSettings());
-            updateControls();
-        });
-
-        el.addEventListener('before.lory.slide', function (event) {
-            if (event.detail.index < event.detail.nextSlide) {
-                index++;
-            } else {
-                index--;
-            }
-        });
-
-        el.addEventListener('after.lory.slide', function () {
-            updateControls();
+        var instance = lory(el, {
+            infinite: 1,
+            enableMouseEvents: true
         });
     }
 
